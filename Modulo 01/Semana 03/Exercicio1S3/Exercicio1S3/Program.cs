3e0a69ba1d8b77e098cc2ab60fae465fcbdc37ac
YuriSousa00 <yuri_sousa00@hotmail.com>
2023-02-21 15:07:13 -0300

    Exercicio1S3 sobre construtores.

---
+++ Modulo 01/Semana 03/Exercicio1S3/Exercicio1S3/Program.cs
@@ -0,0 +1,7 @@
+﻿using Exercicio1S3;
+
+Produto produto1 = new Produto("Calculadora", 5.75, 199);
+Produto produto2 = new Produto();
+
+Console.WriteLine($"O produto1 tem nome: {produto1.nome}, valor: {produto1.preco}, Quantidade: {produto1.quantidade}");
+Console.WriteLine($"O produto1 tem nome: {produto2.nome}, valor: {produto2.preco}, Quantidade: {produto2.quantidade}");

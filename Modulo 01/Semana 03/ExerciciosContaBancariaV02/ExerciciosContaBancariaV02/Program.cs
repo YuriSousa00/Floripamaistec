d215b736a0954390bce77971b40023e85bcdab61
YuriSousa00 <yuri_sousa00@hotmail.com>
2023-02-28 10:03:17 -0300

    Exercicio conta bancariaV2 semana 3.

---
+++ Modulo 01/Semana 03/ExerciciosContaBancariaV02/ExerciciosContaBancariaV02/Program.cs
@@ -0,0 +1,15 @@
+﻿using ExerciciosContaBancariaV02.Classes;
+
+ContaBancariaV02 conta1 = new ContaBancariaV02(1542, 1234, "Bruno Costa", enumContaBancariaV02.POUPANCA);
+ContaBancariaV02 conta2 = new ContaBancariaV02(1545, 1234, "Breno Costa", enumContaBancariaV02.CORRENTE);
+
+
+
+conta1.Depositar(200);
+
+conta1.Transferir(conta2, 50);
+
+conta1.ExibirSaldo();
+//Deve retornar 150
+conta2.ExibirSaldo();
+//Deve retornar 50

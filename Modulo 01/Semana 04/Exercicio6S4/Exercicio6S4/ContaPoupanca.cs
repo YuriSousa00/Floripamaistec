3ec031323be0d3934006f42ea6e2b92177d3d2f6
YuriSousa00 <yuri_sousa00@hotmail.com>
2023-03-01 14:11:33 -0300

    Exercicio 6 semana 4.

---
+++ Modulo 01/Semana 04/Exercicio6S4/Exercicio6S4/ContaPoupanca.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio6S4
+{
+    public class ContaPoupanca : ContaBancaria
+    {
+        // Resposta exercício 03
+        public ContaPoupanca(int numero, int agencia, Cliente cliente) : base(numero, agencia, cliente)
+        {
+            if (cliente.TipoPessoa != EnumTipoPessoa.FISICA)
+            {
+                throw new ArgumentException("Cliente precisa ser do tipo física!");
+            }
+        }
+        // Resposta exercício 06
+        public override void Sacar(decimal valor)
+        {
+            // Taxa de R$ 0,10 por saque
+            valor += 0.10M;
+            base.Sacar(valor);
+        }
+        public override void Transferir(ContaBancaria conta, decimal valor)
+        {
+            // Taxa de R$ 0,05 por transferência
+            valor += 0.05M;
+            base.Transferir(conta, valor);
+        }
+    }
+}
---
+++ Modulo 01/Semana 04/Exercicio6S4/Exercicio6S4/EnumContaBancaria.cs
@@ -0,0 +1,8 @@
+﻿namespace Exercicio6S4
+{
+    public enum EnumTipoPessoa
+    {
+        FISICA,
+        JURIDICA
+    }
+}

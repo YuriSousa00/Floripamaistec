6804c15f67da66dfd99c847d16aaeb442a08e4d8
YuriSousa00 <yuri_sousa00@hotmail.com>
2023-03-13 17:39:20 -0300

    Exercicio 2 Arquivo texto com tratamento de exceções.

---
+++ Modulo 01/Semana 05/TratExcGravandoArquivo/TratExcGravandoArquivo/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+
+    public static void Main(string[] args)
+    {
+        FileInfo arquivo = null;
+        try
+        {
+
+            Console.Write("Digite o nome do arquivo que você vai criar: ");
+            string nomeArquivo = Console.ReadLine();
+
+            arquivo = new FileInfo(nomeArquivo);
+
+            using (StreamWriter gravadorFluxo = File.AppendText(nomeArquivo))
+            {
+                gravadorFluxo.WriteLine("Texto de dentro do arquivo.");
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+        }
+
+        finally
+        {
+            Console.WriteLine($"Encerrando programa...\n");
+           // arquivo = null;
+          //  Console.Clear();
+        }
+    }
+}
+

c1e2f5b5d7bbcc7ea0b34b63260d41ba353b26bb
YuriSousa00 <yuri_sousa00@hotmail.com>
2023-03-13 17:30:36 -0300

    Exercicio 1 tratamento de divisão.

---
+++ Modulo 01/Semana 05/Ex1SoliciteDoisNumeros/Ex1SoliciteDoisNumeros/Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    Console.WriteLine("Insira o primeiro número: ");
+    decimal num1 = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine("Insira o segundo número: ");
+    decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+    decimal resultado = num1 / num2;
+    Console.WriteLine($"Resultado da divisão: {resultado}");
+    
+}
+catch (FormatException ex)
+{
+    Console.WriteLine($"Erro específico capturado: Não é possível usar letras. {ex}.");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine($"Erro específico capturado: Não é possível dividir por 0. {ex}.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro genérico capturado: {ex}.");
+}
+finally
+{
+    int num1 = 0;
+    int num2 = 0;
+}

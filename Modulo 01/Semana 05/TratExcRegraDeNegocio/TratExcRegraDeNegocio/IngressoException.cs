8cc79ecb6a3dc6ce90f2d0ea8b528604f9d27114
YuriSousa00 <yuri_sousa00@hotmail.com>
2023-03-13 17:48:21 -0300

    Exercicio 3 Criação de exceções personalizadas.

---
+++ Modulo 01/Semana 05/TratExcRegraDeNegocio/TratExcRegraDeNegocio/IngressoException.cs
@@ -0,0 +1,18 @@
+﻿namespace TratExcRegraDeNegocio
+{
+    public class IngressoException : Exception
+    {
+        public IngressoException()
+        {
+        }
+        
+        public IngressoException(String msg) : base(msg)
+        {
+        }
+
+        public IngressoException(String msg, Exception causa) : base(msg, causa)
+        {
+        }
+
+    }
+}
---
+++ Modulo 01/Semana 05/TratExcRegraDeNegocio/TratExcRegraDeNegocio/Program.cs
@@ -0,0 +1,26 @@
+﻿using TratExcRegraDeNegocio;
+{
+   
+        Evento evento = new Evento();
+
+        evento.QtdIngressosVendidos = 11;
+        evento.QtdLugares = 12;
+
+        try
+        {
+            if (evento.QtdLugares - evento.QtdIngressosVendidos > 0)
+            {
+                Console.WriteLine("Ingresso validado!");
+                evento.ingressoVendido();
+            }
+            else
+            {
+                throw new IngressoException("Não existem mais lugares disponíveis para o evento");
+            }
+        }
+        catch (IngressoException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    
+}

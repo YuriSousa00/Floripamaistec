6c2d70a66cd8d919abe9a926f470effa698b2bbd
YuriSousa00 <yuri_sousa00@hotmail.com>
2023-03-14 17:54:12 -0300

    Calculadora Tdd sobrecarga de métodos.

---
+++ Modulo 01/Semana 05/CalculadoraTddSC/CalculadoraTddSC/Classes/Calculadora.cs
@@ -0,0 +1,87 @@
+﻿namespace CalculadoraTddSC.Classes
+{
+    public static class Calculadora
+    {
+        public static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Somar(double a, double b)
+        {
+            return a + b;
+        }
+        public static float Somar(float a, float b)
+        {
+            return a + b;
+        }
+        public static decimal Somar(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public static long Somar(long a, long b)
+        {
+            return a + b;
+        }
+        public static int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+        public static double Subtracao(double a, double b)
+        {
+            return a - b;
+        }
+        public static float Subtracao(float a, float b)
+        {
+            return a - b;
+        }
+        public static decimal Subtracao(decimal a, decimal b)
+        {
+            return a - b;
+        }
+        public static long Subtracao(long a, long b)
+        {
+            return a - b;
+        }
+        public static int Multiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Multiplicacao(double a, double b)
+        {
+            return a * b;
+        }
+        public static float Multiplicacao(float a, float b)
+        {
+            return a * b;
+        }
+        public static decimal Multiplicacao(decimal a, decimal b)
+        {
+            return a * b;
+        }
+        public static long Multiplicacao(long a, long b)
+        {
+            return a * b;
+        }
+        public static int Divisao(int a, int b)
+        {
+            return a / b;
+        }
+        public static double Divisao(double a, double b)
+        {
+            return a / b;
+        }
+        public static float Divisao(float a, float b)
+        {
+            return a / b;
+        }
+        public static decimal Divisao(decimal a, decimal b)
+        {
+            return a / b;
+        }
+        public static long Divisao(long a, long b)
+        {
+            return a / b;
+        }
+    }  
+}
+
